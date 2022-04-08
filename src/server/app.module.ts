443d4db41636e89c6c90eac469c174f27f281e87
pulko <fedor.tkachenko@eventinc.de>
2022-04-08 20:52:55 +0200

    refa: remove experiment code

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { ViewModule } from './modules/view/view.module'
 
 @Module({
   imports: [
     ViewModule,
-    GraphQLModule.forRoot<ApolloDriverConfig>({ driver: ApolloDriver }),
   ],
   controllers: [],
   providers: [],
